504e60b462095838e5625b302582d4b56a1c04a2
neomond <neoweise@yahoo.com>
2023-12-30 20:45:42 +0400

    Initial Commit

---
+++ NavBar-programmatic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavBar-programmatic
+//
+//  Created by Nazrin Atayeva on 30.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
